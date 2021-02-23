4b29acc694e5c3e27b2dcca31986e6daca0ce1ef
Łukasz Miłkowski <lukanus@uaznia.net>
2021-02-23 16:51:50 +0100

    Readme + CR corrections

--- health/database/database.go
+++ health/database/database.go
@@ -4,18 +4,18 @@ import "github.com/figment-networks/indexing-engine/metrics"
 
 var (
 	PingMetric = metrics.MustNewHistogramWithTags(metrics.HistogramOptions{
-		Namespace: "indexerhealth",
+		Namespace: "health",
 		Subsystem: "database",
 		Name:      "ping",
 		Desc:      "Duration how long it takes to ping",
-		Tags:      []string{"type"},
+		Tags:      []string{"database_type"},
 	})
 
 	SizeMetric = metrics.MustNewGaugeWithTags(metrics.Options{
-		Namespace: "indexerhealth",
+		Namespace: "health",
 		Subsystem: "database",
 		Name:      "size",
 		Desc:      "Current size of database",
-		Tags:      []string{"type"},
+		Tags:      []string{"database_type"},
 	})
 )
